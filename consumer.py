fff92e6e8b04561960f5b16ca9f0ea2ee98e6800
a.zaiter <a.zaiter@penguinin.com>
2020-10-26 15:13:47 +0300

    emptying project for workshop

--- consumer.py
+++ consumer.py
@@ -3,23 +3,6 @@ from json import loads
 from concurrent.futures import ThreadPoolExecutor, as_completed, thread
 from concurrent.futures._base import TimeoutError
 
-CONSUMERS_QUANTITY = 3
-TOPIC_NAME = "topic_test3"
-running = True
-
-def consumerVisualizer(consumer, id): 
-    for message in consumer:
-        print(f"|{message.value}| received on consumer {id+1}")
 
 if __name__ == "__main__":
-    consumers = [KafkaConsumer(
-        TOPIC_NAME,
-        bootstrap_servers=['localhost:9092'],
-        auto_offset_reset='earliest',
-        group_id=f"group",
-        value_deserializer=lambda x: loads(x.decode('utf-8'))
-    ) for i in range(CONSUMERS_QUANTITY)]
-
-    with ThreadPoolExecutor(max_workers=CONSUMERS_QUANTITY) as executor:
-        list(executor.map(consumerVisualizer, consumers, range(len(consumers))))
     pass
--- producer.py
+++ producer.py
@@ -7,40 +7,6 @@ from concurrent.futures._base import TimeoutError
 from kafka.admin import NewTopic, KafkaAdminClient
 from kafka.errors import TopicAlreadyExistsError
 
-RUNNING_TIMER_IN_SECONDS = 120
-PRODUCERS_QUANTITY = 3
-ITERATION_INTERVAL = 2
-TOPIC_NAME = "topic_test3"
-PARTITIONS_QUANTITY = 3
-
-running = True
-
-def runProducer(producer, id):
-    while running:
-        data = {"value": int(datetime.utcnow().timestamp()), "producer": id+1}
-        producer.send(TOPIC_NAME, value=data)
-        print(f"|{data}| sent")
-        sleep(ITERATION_INTERVAL)
-        pass
-
 if __name__ == "__main__":
-    producers = [KafkaProducer(bootstrap_servers='localhost:9092',
-                               value_serializer=lambda x: dumps(x).encode('utf-8'))
-                 for i in range(PRODUCERS_QUANTITY)]
-    
-    admin_client = KafkaAdminClient(bootstrap_servers="localhost:9092")                                                                                                                                                               
-    topics = admin_client.list_topics()
-    if TOPIC_NAME not in topics:
-        admin_client.create_topics([NewTopic(name=TOPIC_NAME, num_partitions=PARTITIONS_QUANTITY, replication_factor=1)], validate_only=False)
-
-    with ThreadPoolExecutor(max_workers=PRODUCERS_QUANTITY) as executor:
-        try:
-            list(
-                executor.map(runProducer, producers, range(len(producers)),
-                             timeout=RUNNING_TIMER_IN_SECONDS)
-            )
-        except TimeoutError:
-            print("Done executing")
-            running = False
-            pass
+   
     pass
