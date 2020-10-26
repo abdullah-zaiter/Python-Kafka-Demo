788a941ee3618a42d48c2dff875b612924cd6e9b
a.zaiter <a.zaiter@penguinin.com>
2020-10-26 13:37:37 +0300

    Multi threaded producers and intervals

--- producer.py
+++ producer.py
@@ -2,27 +2,36 @@ from time import sleep
 from json import dumps
 from kafka import KafkaProducer
 from datetime import datetime
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from concurrent.futures._base import TimeoutError
 
 PRODUCERS_QUANTITY = 3
-RUN_FOR_SECONDS = 30
+RUNNING_TIMER_IN_SECONDS = 20
+ITERATION_INTERVAL = 2
 
-producers = [ KafkaProducer(bootstrap_servers='localhost:9092',
-                           value_serializer=lambda x: dumps(x).encode('utf-8')) 
-                           for i in range(PRODUCERS_QUANTITY) ]
-
+running = True
 
 def runProducer(producer, id):
-    print(f"here id {id}")
-    while True:
-        data = {"value": datetime.now(), "producer": id}
+    while running:
+        data = {"value": int(datetime.utcnow().timestamp()), "producer": id+1}
         producer.send('topic_test', value=data)
         print(f"|{data}| sent")
-        sleep(2)
-
+        sleep(ITERATION_INTERVAL)
+        pass
 
 if __name__ == "__main__":
+    producers = [KafkaProducer(bootstrap_servers='localhost:9092',
+                               value_serializer=lambda x: dumps(x).encode('utf-8'))
+                 for i in range(PRODUCERS_QUANTITY)]
+
     with ThreadPoolExecutor(max_workers=PRODUCERS_QUANTITY) as executor:
-        # , timeout = RUN_FOR_SECONDS)
-        executor.map(runProducer, producers, range(1, len(producers)+1))
+        try:
+            list(
+                executor.map(runProducer, producers, range(len(producers)),
+                             timeout=RUNNING_TIMER_IN_SECONDS)
+            )
+        except TimeoutError:
+            print("Done executing")
+            running = False
+            pass
     pass
