67d9642694ac28a9d500348c48daabe55e00acc9
a.zaiter <a.zaiter@penguinin.com>
2020-10-26 14:55:47 +0300

    Adding control over topic creation

--- producer.py
+++ producer.py
@@ -4,6 +4,8 @@ from kafka import KafkaProducer
 from datetime import datetime
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from concurrent.futures._base import TimeoutError
+from kafka.admin import NewTopic, KafkaAdminClient
+from kafka.errors import TopicAlreadyExistsError
 
 PRODUCERS_QUANTITY = 3
 RUNNING_TIMER_IN_SECONDS = 20
@@ -23,6 +25,11 @@ if __name__ == "__main__":
     producers = [KafkaProducer(bootstrap_servers='localhost:9092',
                                value_serializer=lambda x: dumps(x).encode('utf-8'))
                  for i in range(PRODUCERS_QUANTITY)]
+    
+    admin_client = KafkaAdminClient(bootstrap_servers="localhost:9092")                                                                                                                                                               
+
+    if "topic_test" not in admin_client.list_topics():
+        admin_client.create_topics([NewTopic(name="topic_test", num_partitions=2, replication_factor=1)], validate_only=False)
 
     with ThreadPoolExecutor(max_workers=PRODUCERS_QUANTITY) as executor:
         try:
