a2a486e696fea7cdb350802a4756c28bc0c9468c
a.zaiter <a.zaiter@penguinin.com>
2020-10-26 14:54:40 +0300

    removing unnecessary code from consumer

--- consumer.py
+++ consumer.py
@@ -5,9 +5,6 @@ from concurrent.futures._base import TimeoutError
 
 CONSUMERS_QUANTITY = 3
 
-RUNNING_TIMER_IN_SECONDS = 15
-
-
 running = True
 
 def consumerVisualizer(consumer, id): 
@@ -19,16 +16,10 @@ if __name__ == "__main__":
         'topic_test',
         bootstrap_servers=['localhost:9092'],
         auto_offset_reset='earliest',
-        group_id=f"group-{i}",
+        group_id=f"group",
         value_deserializer=lambda x: loads(x.decode('utf-8'))
     ) for i in range(CONSUMERS_QUANTITY)]
 
     with ThreadPoolExecutor(max_workers=CONSUMERS_QUANTITY) as executor:
-        try:
-            list(executor.map(consumerVisualizer, consumers, range(len(consumers)), timeout= RUNNING_TIMER_IN_SECONDS))
-        except TimeoutError:
-            print("Done executing")
-            running = False
-            SystemExit(0)
-            pass
+        list(executor.map(consumerVisualizer, consumers, range(len(consumers))))
     pass
