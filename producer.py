0739b42adf13566f8f54f5a15d75c91fc31e6497
a.zaiter <a.zaiter@penguinin.com>
2020-10-26 15:10:45 +0300

    extracting topic name and partitions quant to var

--- producer.py
+++ producer.py
@@ -7,16 +7,18 @@ from concurrent.futures._base import TimeoutError
 from kafka.admin import NewTopic, KafkaAdminClient
 from kafka.errors import TopicAlreadyExistsError
 
+RUNNING_TIMER_IN_SECONDS = 120
 PRODUCERS_QUANTITY = 3
-RUNNING_TIMER_IN_SECONDS = 20
 ITERATION_INTERVAL = 2
+TOPIC_NAME = "topic_test3"
+PARTITIONS_QUANTITY = 3
 
 running = True
 
 def runProducer(producer, id):
     while running:
         data = {"value": int(datetime.utcnow().timestamp()), "producer": id+1}
-        producer.send('topic_test', value=data)
+        producer.send(TOPIC_NAME, value=data)
         print(f"|{data}| sent")
         sleep(ITERATION_INTERVAL)
         pass
@@ -27,9 +29,9 @@ if __name__ == "__main__":
                  for i in range(PRODUCERS_QUANTITY)]
     
     admin_client = KafkaAdminClient(bootstrap_servers="localhost:9092")                                                                                                                                                               
-
-    if "topic_test" not in admin_client.list_topics():
-        admin_client.create_topics([NewTopic(name="topic_test", num_partitions=2, replication_factor=1)], validate_only=False)
+    topics = admin_client.list_topics()
+    if TOPIC_NAME not in topics:
+        admin_client.create_topics([NewTopic(name=TOPIC_NAME, num_partitions=PARTITIONS_QUANTITY, replication_factor=1)], validate_only=False)
 
     with ThreadPoolExecutor(max_workers=PRODUCERS_QUANTITY) as executor:
         try:
